066a60bcb3cca12c9bdba0f743a5e3c1e9d8ea2a
梦宇@极客学院 <xtiqin@163.com>
2015-07-21 22:35:42 +0800

    添加几个小示例

---
+++ SendDataBetweenActivities/app/src/main/java/com/plter/senddatabetweenactivities/AnotherActivity.java
@@ -0,0 +1,48 @@
+package com.plter.senddatabetweenactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AnotherActivity extends AppCompatActivity {
+
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        tv = (TextView) findViewById(R.id.tv);
+
+        String data = getIntent().getStringExtra("data");
+        if (data!=null){
+            tv.setText(data);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_another, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
